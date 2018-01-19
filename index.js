f7912dd22d30765ee6ec586541b19d670280823b
jafaircl <Jonathan@Jonathans-MBP-3.attlocal.net>
2018-01-18 23:47:11 -0500

    cleanup

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const tldr = require('node-tldr')
-const summary = require('node-summary')
 const { Wit, log } = require('node-wit')
 
 /**
